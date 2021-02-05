e20f5a1837a3cf7a2edfc0febab26976e2f2fe25
likhuvhe <likhuvhe@DESKTOP-39O29HI.localdomain>
2021-02-06 01:36:40 +0200

    add task 0.5

---
+++ task_0.5.py
@@ -0,0 +1,7 @@
+def area_of_triangle(side1, side2, side3):
+   semi_perimeter = 0.5 * (side1 + side2 + side3)
+
+   area = (semi_perimeter * (semi_perimeter - side1) * 
+   (semi_perimeter - side2) * (semi_perimeter - side3) ) ** 0.5
+
+   return area
