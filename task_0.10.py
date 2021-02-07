9880ed407a27ffb283a82113fb210683179548a1
likhuvhe <khuvhe.israel@gmail.com>
2021-02-07 16:00:41 +0200

    task 0.10 modified

--- task_0.10.py
+++ task_0.10.py
@@ -1,7 +1,8 @@
-def common_char(str1, str2):
+def common_letter(str1, str2):
     common = list(set(str2) & set(str1))
     
     commons = ""
     for comn in common:
         commons += f"{comn}, "
-    print(commons[:-2])
+    if commons != "":
+        print(f"Common letters: {commons[:-2]}")
