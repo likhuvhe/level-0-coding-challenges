33818525e3e1b005dcaeed58a2c2e09d1c382c7c
likhuvhe <khuvhe.israel@gmail.com>
2021-02-07 04:56:43 +0200

    modify to task 0.9 print string

--- task_0.9.py
+++ task_0.9.py
@@ -3,5 +3,9 @@ import re
 def print_vowels(words):
     pattern = r"[aeiouAEIOU]"
     
-    vowels = re.findall(pattern, words)
-    print(vowels)
+    vowels = list(re.findall(pattern, words))
+    str_vowels = ""
+    for vowel in vowels:
+        str_vowels += f"{vowel}, "
+    
+    print(str_vowels[:-2])
