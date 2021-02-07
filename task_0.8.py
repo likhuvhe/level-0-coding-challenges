ebb146bcfe9c12fe7d8d95b8c3ccb9ed5e132784
likhuvhe <khuvhe.israel@gmail.com>
2021-02-07 15:43:01 +0200

    add task 0.8

---
+++ task_0.8.py
@@ -0,0 +1,22 @@
+def plural_singular(hour, minutes):
+    if hour > 1 and minutes > 1:
+        return f"{hour} hours, {minutes} minutes"
+    elif hour <= 1 and minutes > 1:
+        return f"{hour} hour, {minutes} minutes"
+    elif hour > 1 and minutes <= 1:
+        return f"{hour} hours, {minutes} minute"
+    else:
+        return f"{hour} hour, {minutes} minute"
+        
+def hour_minute(number):
+    hour = 0
+    minutes = 0
+
+    while number:
+        if number >= 60:
+            number -= 60
+            hour += 1
+        else:
+            minutes = number
+            break
+    print(plural_singular(hour, minutes))
