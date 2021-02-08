7c1f22ec38e7760ac7dacacbb1064b1d81f33821
likhuvhe <khuvhe.israel@gmail.com>
2021-02-08 20:27:55 +0200

    use join instead of using for loop when printing

--- task_0.10.py
+++ task_0.10.py
@@ -1,8 +1,3 @@
 def common_letter(str1, str2):
     common = list(set(str2) & set(str1))
-    
-    commons = ""
-    for comn in common:
-        commons += f"{comn}, "
-    if commons != "":
-        print(f"Common letters: {commons[:-2]}")
+    print("".join(common))
